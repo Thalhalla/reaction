5de2cc0d891f29684337b82d8ec51f54933d0b6b
Josh Cox <josh@webhosting.coop>
2017-10-22 10:47:51 -0500

    thalhsuffix

--- .circleci/deploy-thalhalla.sh
+++ .circleci/deploy-thalhalla.sh
@@ -18,7 +18,7 @@ fi
 
 # Thalhalla branch deployment (only runs when a version git tag exists - syntax: "v1.2.3")
 if [[ "$CIRCLE_BRANCH" == "thalhalla" ]]; then
-  VERSION=$(git describe --tags | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+$")
+  VERSION=$(git describe --tags | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+-thalhalla$")
 
   if [[ "$VERSION" ]]; then
     set -e
